fee6e3307ac021c833ab4cdd7ec98badd90d990b
jignuoy <32031963+jignuoy@users.noreply.github.com>
2025-07-21 15:54:44 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,5 +1,10 @@
+import { test1 } from './test.js';
+
 window.myTest = {
   hello: function(name) {
+    return 'Hello,' + name;
+  },
+  hello1: function(name) {
     return 'Hello,' + name;
   }
 }
