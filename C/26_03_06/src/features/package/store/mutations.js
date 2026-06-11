ca2fba0884c8e47860b5d0fe0affca955c94186f
jignuoy <32031963+jignuoy@users.noreply.github.com>
2026-06-11 14:38:21 +0900

    Create mutations.js

---
+++ C/26_03_06/src/features/package/store/mutations.js
@@ -0,0 +1,8 @@
+export default {
+  SET_IS_LOADING_PROMOTION(state, payload) {
+    state.isLoadingPromotion = payload;
+  },
+  SET_PROMOTION_INFO(state, payload) {
+    state.promotionInfo = payload;
+  }
+};
