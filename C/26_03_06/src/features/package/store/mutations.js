3ac822a6be4834373b570cc0656f85b067ecb2dc
jignuoy <32031963+jignuoy@users.noreply.github.com>
2026-07-31 16:14:53 +0900

    Delete C/26_03_06 directory

--- C/26_03_06/src/features/package/store/mutations.js
+++
@@ -1,8 +0,0 @@
-export default {
-  SET_IS_LOADING_PROMOTION(state, payload) {
-    state.isLoadingPromotion = payload;
-  },
-  SET_PROMOTION_INFO(state, payload) {
-    state.promotionInfo = payload;
-  }
-};
